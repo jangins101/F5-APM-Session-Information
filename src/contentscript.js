2b928e2c393eb2813510ab264b67ecb2121381d5
Michael J <jangins101@gmail.com>
2016-03-18 13:19:21 -0400

    removed contentscript because it wasn't being used

--- src/contentscript.js
+++
@@ -1,4 +0,0 @@
-// The background page is asking us for the tab url.
-if (window == top) {
-    chrome.extension.onMessage.addListener(function(req, sender, sendResponse) { sendResponse(window.location); });
-}
