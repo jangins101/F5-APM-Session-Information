a18cab45ef3984fbc6d692507f2cad8de689edc6
Michael J <jangins101@gmail.com>
2016-06-24 14:34:27 -0400

    made options page use angularjs

---
+++ src/js/ngoptions.js
@@ -0,0 +1,110 @@
+'use strict';
+
+var app = angular.module("MyOptions", []);
+/*app.run(function($rootScope) {
+    // Initialization
+    $rootScope.options = (localStorage.options ? JSON.parse(localStorage.options) : defSettings);
+    $rootScope.$watch("options", function(val) {
+        localStorage.options = JSON.stringify(val);
+    })
+
+    $rootScope.data = localStorage.data;
+    $rootScope.$watch("data", function(val) {
+       // If the 'data' field changes, save to localStorage
+        localStorage.data = val;
+    });
+});*/
+
+app.controller('optionsCtrl', function($scope, optionsStorage) {
+    $scope.optionsStorage = optionsStorage;
+    $scope.$watch('optionsStorage.data', function() {
+        console.log("optionsStorage.data watch executed");
+        $scope.options = $scope.optionsStorage.data;
+        //$scope.$apply();
+    });
+
+    $scope.reload = function() {
+        $scope.optionsStorage.load(function(data) {
+            console.log("reload: options loaded");
+            $scope.options = data;
+            $scope.$digest();
+            //$scope.$apply();
+        });
+    }
+    $scope.save = function() {
+        console.log("save: options saved");
+        optionsStorage.save($scope.options);
+    };
+    $scope.cancel = function() {
+        console.log("cancel: options update cancelled");
+        $scope.reload();
+    }
+    $scope.reset = function() {
+        console.log("reset: options reset to default");
+        $scope.optionsStorage.reset();
+        $scope.reload();
+    }
+
+    // Initally load the stuff
+    $scope.reload();
+});
+
+app.service('optionsStorage', function ($q) {
+    var _this = this;
+    this.defaults = {
+            // Enable debug header on requests for a given domain
+            enableDebug: false,
+            debugHeaderName: "DEBUG_F5",
+            debugHeaderValue: "1",
+            debugDomains: "",
+            debugDomainsParsed: function() {
+                return this.debugDomains.split(/,|\n/g);
+            },
+
+            // Enable the extension on a given header value for the Server header
+            enableOnHeaderServer: false,
+            onHeaderServerValue: "Bigip"
+        };
+    this.data = null; //angular.copy(this.defaults);
+
+    this.load = function(callback) {
+        console.log("storage.load: getting options from storage")
+        chrome.storage.sync.get('options', function(keys) {
+            console.log("storage.load.get: options retireved");
+            _this.data = keys.options ? keys.options : angular.copy(_this.defaults);
+            console.log(_this.data);
+
+            // Execute the callback
+            if (callback) callback(_this.data);
+        });
+    };
+    this.save = function(data) {
+        console.log("storage.save: saving options");
+        _this.data = data;
+        chrome.storage.sync.set({options: _this.data}, function() {
+            console.log("storage.save.set: options saved");
+           console.log("Saved options to Chrome storage");
+        });
+        console.log(data);
+    };
+    this.reset = function(callback) {
+        console.log("storage.reset: resetting options to default");
+        _this.save(angular.copy(_this.defaults));
+    };
+
+    // Load the options
+    this.load();
+});
+
+
+
+
+/*
+// Example code to fetch current tab and save to $scope
+chrome.tabs.getCurrent(function(tab) {
+   // $apply() is needed to trigger the digest cycle to refresh the UI
+    $scope.$apply(function(){
+        $scope.title = tab.title; //Executed in digest loop.
+    });
+});
+*/
